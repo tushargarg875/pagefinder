43b3fca5841398ca215238de729d63532a73d11f
Tushar <tushargarg875@gmail.com>
2020-05-14 13:41:11 +0530

    Added waiting for react.js to work

--- page_puller.py
+++ page_puller.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 search_input = input("Enter what you want to search: ")
 if len(search_input)<1:
@@ -26,10 +29,13 @@ CHROME_DRIVER_PATH = os.getcwd() + "\chromedriver"
 
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH,chrome_options=chrome_options)
 driver.get(url)
-elements = driver.find_elements(By.TAG_NAME, 'div')
-for e in elements:
-    try:
-        print (e.text)
-    except: 
-        pass
-  
+delay = 3 # seconds
+try:
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#react-wayback-search .search-result-container li')))
+    print ("Page is ready!")
+except TimeoutException:
+    print ("Loading took too much time!")
+
+stuff = driver.find_elements_by_css_selector("#react-wayback-search .search-result-container li")
+for item in stuff:
+    print(item.get_attribute("class"))
